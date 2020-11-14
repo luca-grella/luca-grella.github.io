f6836b2ae0b39ad20d17693603e30259e05ad1a0
Luca Grella <luca.grella@me.com>
2020-11-14 19:42:20 +0100

    tesi

--- tesi/demo/instance_creator.js
+++ tesi/demo/instance_creator.js
@@ -1,30 +1,28 @@
 /////SEARCH FUNCTION/////////////////////////////
     $(function () {
               var to = false;
-              $('#demo_q').keyup(function () {
+              $('#demo_q2').keyup(function () {
                 if(to) { clearTimeout(to); }
                 to = setTimeout(function () {
-                  var v = $('#demo_q').val();
-                  $('#jstree_demo').jstree(true).search(v);
+                  var v = $('#demo_q2').val();
+                  $('#jstree_instances').jstree(true).search(v);
                 }, 250);
               });
 ////////////////////////////////////////////////
 	
 	//CDT POPULATION
-	$('#jstree_demo').jstree({
+	$('#jstree_instances').jstree({
   "core" : {
     "animation" : 0,
     "check_callback" : true,
     "themes" : { "stripes" : true },
     'data' : {
-				"url" : "./root.json",
+				"url" : "./tree_structure.json",
 				"dataType" : "json" 
 			}
   },
   "types" : {
     "#" : {
-      "max_children" : 512,
-      "max_depth" : 512,
       "valid_children" : ["root"]
     },
     "root" : {
@@ -45,13 +43,14 @@
     }
   },
   "plugins" : [
-    "contextmenu", "dnd", "search",
+    "contextmenu", "search",
     "state", "types", "wholerow"
   ],
   "contextmenu" : {
     "items" : function(node) {
             var items = $.jstree.defaults.contextmenu.items();
             items.create = false;
+            items.ccp = false;
 
             return items;
         }
@@ -71,10 +70,10 @@ function encode( s ) {
     return new Uint8Array( out );
 }
 
-var savejson = document.getElementById( 'savejson' );
+var savejson = document.getElementById( 'savejson2' );
 savejson.addEventListener( 'click', function() {
     
-	var v = $('#jstree_demo').jstree(true).get_json('#', {flat:false,no_state:true, no_data:false, no_type:true, no_icon:true, no_li_attr:true, no_a_attr:true})
+	var v = $('#jstree_instances').jstree(true).get_json('#', {flat:false,no_state:true, no_data:false, no_type:true, no_icon:true, no_li_attr:true, no_a_attr:true})
 
     var data = encode( JSON.stringify(v, null, 4) );
 
@@ -85,208 +84,91 @@ savejson.addEventListener( 'click', function() {
     url = URL.createObjectURL( blob );
     var link = document.createElement( 'a' );
     link.setAttribute( 'href', url );
-    link.setAttribute( 'download', 'root.json' );
+    link.setAttribute( 'download', 'instances.json' );
     
     var event = document.createEvent( 'MouseEvents' );
     event.initMouseEvent( 'click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
     link.dispatchEvent( event );
 });
 
-//BUTTONS FUNCTION: ALL NODE CREATION FUNCTION ACT ON THE SELECTED NODE
-
-//ATTRIBUTE CREATION
-var createattribute = document.getElementById( 'createattribute' );
-createattribute.addEventListener( 'click', function() {
-
-        var ref = $('#jstree_demo').jstree(true),
-        sel = ref.get_selected();
-        if(!sel.length) { return false; }
-        var i;
-			  for (i = 0; i < sel.length; i++) { 
-			  sel_id = sel[i];
-        var v = $('#jstree_demo').jstree(true).get_json(sel_id);
-
-        var j;
-        var checkchild = false;
-        for (j = 0; j < v.children.length; j++){
-            checkchild = checkchild || 
-            v.children[j].type == 'concept' || 
-            v.children[j].type == 'dimension'
-        }
-
-        if (JSON.stringify(v.children) != '[]') {
-          
-          if (checkchild) {//Do Nothing
-          }
-          else {
-              asd = ref.create_node(sel_id, {"type":"attribute"});
-              if(asd) {
-              ref.edit(asd);
-              }
-                }
-
-        }
-        else {
-              asd = ref.create_node(sel_id, {"type":"attribute"});
-              if(asd) {
-              ref.edit(asd);
-              }
-                }
-			                           }
-            });
-
-//CONCEPT CREATION
-var createconcept = document.getElementById( 'createconcept' );
-createconcept.addEventListener( 'click', function() {
-
-        var ref = $('#jstree_demo').jstree(true),
-        sel = ref.get_selected();
-        if(!sel.length) { return false; }
-        var i;
-        for (i = 0; i < sel.length; i++) { 
-        sel_id = sel[i];
-        var v = $('#jstree_demo').jstree(true).get_json(sel_id);
-
-        var j;
-        var checkchild = false;
-        for (j = 0; j < v.children.length; j++){
-            checkchild = checkchild || 
-            v.children[j].type == 'attribute'
-        }
-
-        if (JSON.stringify(v.children) != '[]') {
-          
-          if (checkchild) {//Do Nothing
-          }
-          else {
-              asd = ref.create_node(sel_id, {"type":"concept"});
-              if(asd) {
-              ref.edit(asd);
-              }
-                }
-
-        }
-        else {
-              asd = ref.create_node(sel_id, {"type":"concept"});
-              if(asd) {
-              ref.edit(asd);
-              }
-                }
-                                 }
-            });
-
-//DIMENSION CREATION
-var createdimension = document.getElementById( 'createdimension' );
-createdimension.addEventListener( 'click', function() {
-
-        var ref = $('#jstree_demo').jstree(true),
-        sel = ref.get_selected();
-        if(!sel.length) { return false; }
-        var i;
-        for (i = 0; i < sel.length; i++) { 
-        sel_id = sel[i];
-        var v = $('#jstree_demo').jstree(true).get_json(sel_id);
-
-        var j;
-        var checkchild = false;
-        for (j = 0; j < v.children.length; j++){
-            checkchild = checkchild || 
-            v.children[j].type == 'attribute'
-        }
-
-        if (JSON.stringify(v.children) != '[]') {
-          
-          if (checkchild) {//Do Nothing
-          }
-          else {
-              asd = ref.create_node(sel_id, {"type":"dimension"});
-              if(asd) {
-              ref.edit(asd);
-              }
-                }
-
-        }
-        else {
-              asd = ref.create_node(sel_id, {"type":"dimension"});
-              if(asd) {
-              ref.edit(asd);
-              }
-                }
-                                 }
-            });
-
-//ROOT CREATION
-var createroot = document.getElementById( 'createroot' );
-createroot.addEventListener( 'click', function() {
-
-              var ref = $('#jstree_demo').jstree(true),
-              sel = ref.create_node("#", {"type":"root"});
-              if(sel) {
-                ref.edit(sel);
-              }
-            });
-
 //INFORTUNIO NODE DUPLICATION
 var duplicateroot = document.getElementById( 'duplicateroot' );
 duplicateroot.addEventListener( 'click', function() {
 
-              var ref = $('#jstree_demo').jstree('copy', '1');
-              ref = $('#jstree_demo').jstree('paste', '#', 'last');
-            });
-
-//COPY SELECTED NODE FUNCTION
-var copynode = document.getElementById( 'copynode' );
-copynode.addEventListener( 'click', function() {
-
-		 	var ref = $('#jstree_demo').jstree(true),
-        	sel = ref.get_selected();
-              var cop = $('#jstree_demo').jstree('copy', sel);
-            });
-
-//PASTE IN SELECTED NODE FUNCTION
-var pastenode = document.getElementById( 'pastenode' );
-pastenode.addEventListener( 'click', function() {
-
-		 	var ref = $('#jstree_demo').jstree(true),
-        	sel = ref.get_selected();
-              var pas = $('#jstree_demo').jstree('paste', sel, 'last');
+              var ref = $('#jstree_instances').jstree('copy', '1');
+              ref = $('#jstree_instances').jstree('paste', '#', 'last');
             });
 
 //RENAME SELECTED NODE FUNCTION
-var renamenode = document.getElementById( 'renamenode' );
+var renamenode = document.getElementById( 'renamenode2' );
 renamenode.addEventListener( 'click', function() {
 
-              var ref = $('#jstree_demo').jstree(true),
-                sel = ref.get_selected();
+              var ref = $('#jstree_instances').jstree(true),
+              sel = ref.get_selected();
+              var sel_id = 0;
+              var can_rename = false;
+
+
+              for (i = 0; i < sel.length; i++) 
+              { 
+                if($('#jstree_instances').jstree(true).get_json(sel[i]).type == 'attribute')
+                {
+                  can_rename = true;
+                  if(i < sel_id){sel_id = i}
+                }
+              }
               if(!sel.length) { return false; }
-              sel = sel[0];
-              ref.edit(sel);
+              sel = sel[sel_id];
+              if(can_rename)
+              {
+                ref.edit(sel);
+              }
+              else 
+              {
+                //Do Nothing
+              }              
             });
 
 //DELETE SELECTED NODE FUNCTION
-var deletenode = document.getElementById( 'deletenode' );
+var deletenode = document.getElementById( 'deletenode2' );
 deletenode.addEventListener( 'click', function() {
 
-              var ref = $('#jstree_demo').jstree(true),
-                sel = ref.get_selected();
+              var ref = $('#jstree_instances').jstree(true),
+              sel = ref.get_selected();
+              var no_delete = false;
+              for (i = 0; i < sel.length; i++) 
+              { 
+                sel_id = sel[i];
+                if($('#jstree_instances').jstree(true).get_json(sel_id).type == 'root')
+                {
+                  no_delete = true
+                }
+              }
               if(!sel.length) { return false; }
-              ref.delete_node(sel);
+              if(no_delete)
+              {
+                //Do Nothing
+              }
+              else 
+              {
+                ref.delete_node(sel);
+              }
             });
 
 //EXPAND TREE
-var openallnodes = document.getElementById( 'openallnodes' );
+var openallnodes = document.getElementById( 'openallnodes2' );
 openallnodes.addEventListener( 'click', function() {
 
-              $('#jstree_demo').jstree('open_all')
+              $('#jstree_instances').jstree('open_all')
             });
 
 //REDUCE TREE
-var closeallnodes = document.getElementById( 'closeallnodes' );
+var closeallnodes = document.getElementById( 'closeallnodes2' );
 closeallnodes.addEventListener( 'click', function() {
 
-	var node_to_hide = $("#jstree_demo").jstree(true).get_node('1');   
-	hiding = $("#jstree_demo").jstree(true).hide_node(node_to_hide);
-              $('#jstree_demo').jstree('close_all');
+	var node_to_hide = $("#jstree_instances").jstree(true).get_node('1');   
+	hiding = $("#jstree_instances").jstree(true).hide_node(node_to_hide);
+              $('#jstree_instances').jstree('close_all');
             });
 
 //TRIGGERING NODE1 HIDING (FOR "INFORTUNIO" DUPLICATION) AND COMPACT VIEW ON OPEN
