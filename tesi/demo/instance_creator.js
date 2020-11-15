05a67c0fb68f2ea58d17393f2f1ad580b3ede411
Luca Grella <luca.grella@me.com>
2020-11-15 15:20:37 +0100

    tesi

--- tesi/demo/instance_creator.js
+++ tesi/demo/instance_creator.js
@@ -181,6 +181,6 @@ function provaHide() {
   var node_to_hide = $("#jstree_instances").jstree(true).get_node('1');   
 	hiding = $("#jstree_instances").jstree(true).hide_node(node_to_hide);
 };
-
+ 
 window.onload = provaHide();
 /*setInterval(provaHide, 1000);*/
