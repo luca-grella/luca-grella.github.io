0563138cdea80952c050776ef7395af7fdf9f51d
Luca Grella <luca.grella@me.com>
2020-11-15 18:26:52 +0100

    tesi

--- tesi/demo/instance_creator.js
+++ tesi/demo/instance_creator.js
@@ -13,51 +13,6 @@ document.getElementById('import').onclick = function() {
   console.log(e);
     var result = JSON.parse(e.target.result);
     var resultsjson = JSON.stringify(result, null, 2);
-///////////////////////////////////////
-$('#jstree_instances').jstree({
-  "core" : {
-    "animation" : 0,
-    "check_callback" : true,
-    "themes" : { "stripes" : true },
-    'data' : resultsjson
-  },
-  "types" : {
-    "#" : {
-      "valid_children" : ["root"]
-    },
-    "root" : {
-      "icon" : "./icons/root.png",
-      "valid_children" : ["dimension"]
-    },
-    "dimension" : {
-      "icon" : "./icons/dimension.png",
-      "valid_children" : ["concept","attribute"]
-    },
-    "concept" : {
-      "icon" : "./icons/concept.png",
-      "valid_children" : ["dimension","attribute"]
-    },
-    "attribute" : {
-      "icon" : "./icons/attribute.png",
-      "valid_children" : []
-    }
-  },
-  "plugins" : [
-    "contextmenu", "search",
-    "state", "types", "wholerow"
-  ],
-  "contextmenu" : {
-    "items" : function(node) {
-            var items = $.jstree.defaults.contextmenu.items();
-            items.create = false;
-            items.ccp = false;
-            items.remove = false;
-
-            return items;
-        }
-  }
-});
-//////////////////////////////////////
 		document.getElementById('result').value = resultsjson;
   }
   
@@ -75,7 +30,68 @@ $('#jstree_instances').jstree({
                 }, 250);
               });
 ////////////////////////////////////////////////
-	
+$.when( document.getElementById('import').onclick = function() {
+	var files = document.getElementById('selectFiles').files;
+  console.log(files);
+  if (files.length <= 0) {
+    return false;
+  }
+  
+  var fr = new FileReader();
+  
+  fr.onload = function(e) { 
+  console.log(e);
+    var result = JSON.parse(e.target.result);
+    var resultsjson = JSON.stringify(result, null, 2);
+		document.getElementById('result').value = resultsjson;
+  }
+  
+  fr.readAsText(files.item(0));
+} ).done(function() {
+  $('#jstree_instances').jstree({
+    "core" : {
+      "animation" : 0,
+      "check_callback" : true,
+      "themes" : { "stripes" : true },
+      'data' : resultsjson
+    },
+    "types" : {
+      "#" : {
+        "valid_children" : ["root"]
+      },
+      "root" : {
+        "icon" : "./icons/root.png",
+        "valid_children" : ["dimension"]
+      },
+      "dimension" : {
+        "icon" : "./icons/dimension.png",
+        "valid_children" : ["concept","attribute"]
+      },
+      "concept" : {
+        "icon" : "./icons/concept.png",
+        "valid_children" : ["dimension","attribute"]
+      },
+      "attribute" : {
+        "icon" : "./icons/attribute.png",
+        "valid_children" : []
+      }
+    },
+    "plugins" : [
+      "contextmenu", "search",
+      "state", "types", "wholerow"
+    ],
+    "contextmenu" : {
+      "items" : function(node) {
+              var items = $.jstree.defaults.contextmenu.items();
+              items.create = false;
+              items.ccp = false;
+              items.remove = false;
+  
+              return items;
+          }
+    }
+  })
+});
 	//CDT POPULATION
 	$('#jstree_instances').jstree({
   "core" : {
