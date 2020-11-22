689461ae7a92a20a342aaa03d2d84f841f43a1ff
Luca Grella <luca.grella@me.com>
2020-11-22 23:27:19 +0100

    tesi

---
+++ tesi/demo/js/cryptography.js
@@ -0,0 +1,22 @@
+/*var input = document.getElementById("secret");
+input.addEventListener("keyup", function(event){
+      if (event.keyCode === 13) 
+      {
+        event.preventDefault();
+        document.getElementById("EnterButton").click();
+      }
+    });
+
+function ProtectedArea() {
+  if(document.getElementById('secret').value == "tree_structure_creator" ||
+     document.getElementById('secret').value == "instance_creator" ||
+     document.getElementById('secret').value == "tree_viewer"){
+      location.href = document.getElementById('secret').value + ".html"
+     }
+  else 
+  {
+      document.getElementById("pswerror").innerHTML = "Password Error!"
+  }
+    };*/
+
+//https://www.smartgb.com/free_encrypthtml.php
