cf482de1fe809607b985468234c922bea56bc60d
Luca Grella <luca.grella@me.com>
2020-11-23 19:13:20 +0100

    tesi

--- tesi/demo/js/instance_creator.js
+++ tesi/demo/js/instance_creator.js
@@ -100,13 +100,12 @@ var savejson = document.getElementById( 'savejson2' );
 savejson.addEventListener( 'click', function() {
     
 	var v = $('#jstree_instances').jstree(true).get_json('#', {flat:false,no_state:true, no_data:false, no_type:true, no_icon:true, no_li_attr:true, no_a_attr:true})
-  var w = $('#jstree_instances').jstree(true).get_node(v[1].id);
-  var u = w;
+  var u = v[1];
   if(v.length >= 3)
   {
     for(i=2; i<v.length; i++)
     {
-      u = u.concat(v[i]);
+      u = u+v[i];
     }
   }
     var data = encode( JSON.stringify(u, null, 4) );
