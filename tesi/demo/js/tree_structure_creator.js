09c54e9239aed6beac621bf186006b526e8ee496
Luca Grella <luca.grella@me.com>
2020-11-23 13:59:28 +0100

    tesi

--- tesi/demo/js/tree_structure_creator.js
+++ tesi/demo/js/tree_structure_creator.js
@@ -241,7 +241,7 @@ createroot.addEventListener( 'click', function() {
 
               var ref = $('#jstree_demo').jstree(true),
               sel = ref.create_node("#", {"type":"root"});
-              sel.id = "bubba";
+              sel[0].id = "bubba";
               if(sel) {
                 ref.edit(sel);
               }
