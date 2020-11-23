b9eb8b88a3d032c874af0f095bc38815a625e6db
Luca Grella <luca.grella@me.com>
2020-11-23 12:05:14 +0100

    tesi

--- tesi/demo/js/instance_creator.js
+++ tesi/demo/js/instance_creator.js
@@ -130,7 +130,7 @@ appenddbandsave.addEventListener( 'click', function() {
     var data_new = data + new_data_string;
     var data_final = data_new.replace("][", ",");
   
-    var blob = new Blob( [ new_data_string ], {
+    var blob = new Blob( [ data_final ], {
         type: 'application/octet-stream'
     });
     
