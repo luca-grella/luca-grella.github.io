8e03011af6c548e45d9d567083ab8c34848e20a8
Luca Grella <luca.grella@me.com>
2020-11-23 19:20:01 +0100

    tesi

--- tesi/demo/js/instance_creator.js
+++ tesi/demo/js/instance_creator.js
@@ -107,7 +107,7 @@ savejson.addEventListener( 'click', function() {
     {
       u = u+v[i];
     }
-    u = JSON.parse(u);
+    u = JSON.stringify(u);
   }
     var data = encode( JSON.stringify(u, null, 4) );
 
