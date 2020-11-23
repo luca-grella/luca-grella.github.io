ffe0d1861059621a1dfaa912c2d699882cd61e38
Luca Grella <luca.grella@me.com>
2020-11-23 11:33:20 +0100

    tesi

--- tesi/demo/js/instance_creator.js
+++ tesi/demo/js/instance_creator.js
@@ -117,21 +117,27 @@ appenddbandsave.addEventListener( 'click', function() {
 	var v = $('#jstree_instances').jstree(true).get_json('#', {flat:false,no_state:true, no_data:false, no_type:true, no_icon:true, no_li_attr:true, no_a_attr:true})
 
     var data = encode( JSON.stringify(v, null, 4) );
-    /*var new_data = loadJSON('./instances.json');*/
-    //
-    var request = new XMLHttpRequest();
-    request.open("GET", "./instances.json", false);
-    //request.send(null)
-    var new_data = JSON.parse(request);
-    //alert (my_JSON_object.result[0]);
-    //
-    //var new_data = JSON.parse('./instances.json');
-    /*var provaprovaprova = JSON.parse(new_data);*/
+    
+    var prova = function loadJSON(callback) {   
+
+      var xobj = new XMLHttpRequest();
+          xobj.overrideMimeType("application/json");
+      xobj.open('GET', './instances.json', true); // Replace 'my_data' with the path to your file
+      xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+              // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+              callback(xobj.responseText);
+            }
+      };
+      xobj.send(null);  
+   }
+    
+    var new_data;
     var new_data_string = encode( JSON.stringify(new_data, null, 4) );
     var data_new = data + new_data_string;
     var data_final = data_new.replace("][", ",");
   
-    var blob = new Blob( [ new_data_string ], {
+    var blob = new Blob( [ prova ], {
         type: 'application/octet-stream'
     });
     
